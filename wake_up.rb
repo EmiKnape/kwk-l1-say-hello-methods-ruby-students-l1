57fa9ba95d26637244e4443ad4b735ce8167bb23
EmiKnape <enknape@aol.com>
2018-06-05 16:46:15 +0000

    automatically backed up by learn

---
+++ wake_up.rb
@@ -0,0 +1,8 @@
+def wake_up
+  puts "I wake up to my alarm blasting First Impressions from First Date"
+  puts "I shut off my alarm after jamming out to the music for a little bit"
+  puts "I take a shower to clean my greasy hair"
+  puts "I get dressed and ready for the day"
+  puts "I eat a delicious breakfast, usually consisting of eggs and a Starbucks Refresher"
+  puts "Then I start the day by pulling out my iPad and watch as many episodes of The Flash as I can"
+end
