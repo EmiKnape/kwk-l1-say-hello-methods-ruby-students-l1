4ed89bac919d4951a1121177106161ec6d7b6485
EmiKnape <enknape@aol.com>
2018-06-05 16:52:11 +0000

    automatically backed up by learn

--- wake_up.rb
+++ wake_up.rb
@@ -3,6 +3,9 @@ def wake_up
   puts "I shut off my alarm after jamming out to the music for a little bit"
   puts "I take a shower to clean my greasy hair"
   puts "I get dressed and ready for the day"
+  5.times do
+  puts "I choose what scrunchie I want to wear that day"
+end
   puts "I eat a delicious breakfast, usually consisting of eggs and a Starbucks Refresher"
   puts "Then I start the day by pulling out my iPad and watch as many episodes of The Flash as I can"
 end
